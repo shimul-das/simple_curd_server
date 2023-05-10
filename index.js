8d3d0f207333008fd1b2efb1182d31a8b17cce25
Shimul Das <shimul.191002110@gmail.com>
2023-05-10 08:26:25 +0600

    get specific data from mongo database

--- index.js
+++ index.js
@@ -45,6 +45,12 @@ async function run() {
             res.send(result);
 
         })
+        app.get('/user/:id',async(req,res)=>{
+            const id=req.params.id;
+            const query={_id: new ObjectId(id)}
+            const result = await UserCollection.findOne(query)
+            res.send(result);
+        })
         
         app.post('/user', async(req, res) => {
             const user = req.body;
